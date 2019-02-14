1e8a53f7a3e362dafeb3af570607c0d02a68383f
Alexey Kreshchuk <akrsch@gmail.com>
2019-02-14 05:08:06 +0300

    add GSL

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
-#include <vector>
 #include <fmt/core.h>
+#include <vector>
 
-int main(int, char**) {
-    std::vector vec {1,2,3};
-    std::vector vec2{vec,vec};
+int main(int, char**)
+{
+    std::vector vec{1, 2, 3};
+    std::vector vec2{vec, vec};
     fmt::print("Hello, world!\n");
     std::string s{fmt::format("{} {}", 1, 2)};
     return 0;
